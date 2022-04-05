730e5dd0ffef621c40ed05ae0e82cdb113c1c02b
KSANARH <96589733+KSANARH@users.noreply.github.com>
2022-04-05 22:23:29 +0300

    Add files via upload

--- js/menu_burger.js
+++ js/menu_burger.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slider = new ChiefSlider('.slider', {
-        loop: true,
-        autoplay: true,
-        interval: 7000,
-      });
-    });
+let menu_burger = document.querySelector('.menu-burger');
+let menu = document.querySelector('.menu');
+let back = document.querySelector('body');
+let menu_list = document.querySelector('.menu__list');
 
-    let menu_burger = document.querySelector('.menu-burger');
-    let menu = document.querySelector('.menu');
-    let back = document.querySelector('body');
-    let menu_list = document.querySelector('.menu__list');
-    
-    menu_burger.onclick = function(){
-        menu_burger.classList.toggle('active');
-        menu.classList.toggle('active');
-        back.classList.toggle('lock');
-    }
-    
+let menu__item = document.querySelectorAll('.menu__item');
+ 
+menu_burger.onclick = function(){
+menu_burger.classList.toggle('active');
+menu.classList.toggle('active');
+back.classList.toggle('lock');
+};
+
+[].forEach.call(menu__item, function(el) {
+  el.onclick = function(e) {
+  menu_burger.classList.remove('active');
+  menu.classList.remove('active');
+  back.classList.remove('lock');
+  }
+});
